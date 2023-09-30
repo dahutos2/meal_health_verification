236e4f9beb534c6f563b71fe48bb55485db76d3d
Atsuhiro.uehara <try.again.31.06@gmail.com>
2023-09-30 16:29:48 +0900

    uehara ホームページへ名前変更

--- lib/widget/home/home_widget.dart
+++ lib/widget/home/home_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SampleListView extends ConsumerWidget {
-  const SampleListView({super.key});
+class HomeWidget extends ConsumerWidget {
+  const HomeWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
