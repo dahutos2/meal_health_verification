8c5821d6f9eea85f88a976173c40fd47e2408b01
Atsuhiro.uehara <try.again.31.06@gmail.com>
2023-09-30 16:14:05 +0900

    uehara ホームページへ名前変更と多言語対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'index.dart';
 
@@ -27,7 +27,7 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       localizationsDelegates: L10n.localizationsDelegates,
       supportedLocales: L10n.supportedLocales,
-      onGenerateRoute: (_) => RouteType.fadeIn(nextPage: const Page01()),
+      onGenerateRoute: (_) => RouteType.fadeIn(nextPage: const HomePage()),
     );
   }
 }
