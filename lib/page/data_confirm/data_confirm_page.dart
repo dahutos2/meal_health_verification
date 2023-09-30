ade2742020e0942c0b4fd77a32f482a77c1077c2
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-09-30 16:43:24 +0900

    グラフと食事履歴表示のエリア分割

--- lib/page/data_confirm/data_confirm_page.dart
+++ lib/page/data_confirm/data_confirm_page.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import '../../widget/page03/widget_page03.dart';
 import '../common/page_common.dart';
 
 class DataConfirm extends StatelessWidget {
   const DataConfirm({super.key});
 
   @override
-  Widget build(BuildContext context) => BasePage(
+  Widget build(BuildContext context) {
+    return BasePage(
       headerTitle: L10n.of(context)!.dataConfirmTitle,
-      body: const Page03View());
+      body: const Column(
+        children: [
+          Expanded(child: Text('チャート部')),
+          Expanded(child: Text('食事履歴表示部')),
+        ],
+      ),
+    );
+  }
 }
