21f8445887753ba0758c550f3bbf9033d078ebab
daichi.okumura <dahutos2@gmail.com>
2023-10-01 01:29:11 +0900

    コードの安全性を向上

--- lib/widget/camera/pause_camera/pause_camera.dart
+++ lib/widget/camera/pause_camera/pause_camera.dart
@@ -59,10 +59,13 @@ class _PauseCameraState extends State<PauseCamera> {
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      final tempDir = await getTemporaryDirectory();
+      final osTempDir = await getTemporaryDirectory();
+      final Directory tempDir = Directory('${osTempDir.path}/temp_image/');
+
+      tempDir.createSync(recursive: true);
       await _startCamera();
       setState(() {
-        _tempDir = '${tempDir.path}/temp_image/';
+        _tempDir = tempDir.path;
         _errorMessage = L10n.of(context)!.pauseCameraStart;
       });
     });
@@ -171,6 +174,7 @@ class _PauseCameraState extends State<PauseCamera> {
   }
 
   void _deleteImages() {
+    if (!mounted) return;
     final files = Directory(_tempDir).listSync();
     for (var entity in files) {
       if (entity is File) {
