8e055aa76ad6bb0ca611c145fefbbe7a167d6400
daichi.okumura <dahutos2@gmail.com>
2023-10-09 23:04:18 +0900

    グラフを変更

--- docs/simulation/script.js
+++ docs/simulation/script.js
@@ -1,35 +1,44 @@
+let myChart;
+
 function calculate() {
     const length = document.getElementById('length').value;
     const times = document.getElementById('times').value;
     const data = [];
-    const progressDiv = document.getElementById('progress');
+
+    // 既存のグラフが存在する場合は破棄
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    if (1 > length || length > 100) return;
+    if (100 > times || times > 10000) return;
+
     for (let i = 0; i < times; i++) {
         const array = Array.from({ length: length }, () => Math.round(Math.random() * 100));
         const mean = array.reduce((sum, value) => sum + value, 0) / length;
         const variance = array.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) / length;
         const stdDev = Math.sqrt(variance);
         data.push(stdDev);
-
-        // 進捗率を更新
-        if (i % 100 === 0) {  // 100回ごとに進捗を更新
-            const progress = ((i / times) * 100).toFixed(2);
-            progressDiv.innerText = `進捗: ${progress}%`;
-        }
     }
     const frequency = {};
     data.forEach(value => {
         const key = value.toFixed(1);
         frequency[key] = (frequency[key] || 0) + 1;
     });
-    const labels = Object.keys(frequency).map(key => parseFloat(key)).filter(value => value <= 50);
-    const chartData = labels.map(label => frequency[label.toFixed(1)]);
+
+    // 0から50までのラベルを作成し、小数点第1位までの値を取得
+    const labels = Array.from({ length: 501 }, (_, i) => parseFloat((i / 10).toFixed(1))).filter(value => value <= 50);
+
+    // 存在しない値は0として扱う
+    const chartData = labels.map(label => frequency[String(label)] || 0);
+
     const ctx = document.getElementById('chart').getContext('2d');
-    new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: labels,
             datasets: [{
-                label: 'Frequency',
+                label: '標準偏差の分布',
                 data: chartData,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
@@ -44,14 +53,14 @@ function calculate() {
                     max: 50,
                     title: {
                         display: true,
-                        text: 'Standard Deviation'
+                        text: '標準偏差'
                     }
                 },
                 y: {
                     beginAtZero: true,
                     title: {
                         display: true,
-                        text: 'Count'
+                        text: '回数'
                     }
                 }
             }
